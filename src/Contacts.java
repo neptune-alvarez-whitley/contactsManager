3a06f581d59c1ec07864a4fa1e01855d69a4d281
Austin Whitley <whitley.austin01@gmail.com>
2021-06-10 16:55:28 -0500

    added a method to test if a contact exists

--- src/Contacts.java
+++ src/Contacts.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,12 +11,38 @@ import java.util.Scanner;
 
 public class Contacts{
 
+    public void addNewContact(Path location, ArrayList<String> contactList, String userContact){
+        ArrayList<String> currentList = new ArrayList<>();
+        try {
+            currentList = (ArrayList<String>) Files.readAllLines(location);
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+        try {
+            //Create a reader that will test every line of our text file against the new String the user wants to add
+            //if the string IS found inside our file, it gets thrown out and nothing is added.
+            //If it is NOT found then it gets added to the list
+            BufferedReader reader = new BufferedReader(new FileReader(new File(String.valueOf(location))));
+            String line;
+            while((line = reader.readLine()) != null){
+                if(currentList.contains(userContact)){
+                    System.out.println("This contact already exists.");
+                    break;
+                }
+                write(location, contactList, StandardOpenOption.APPEND);
+                break;
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        System.out.println("Hey there all you cool cats and kittens");
+    }
+
     public void write(Path location, ArrayList<String> information, StandardOpenOption option){
         try{
             //2 arguments for File.write
             // the path to the file, the data we want to write
             Files.write(location, information, option);
-
         }catch(IOException ioe){
             ioe.printStackTrace();
         }
@@ -75,42 +102,50 @@ public class Contacts{
 //        benny.readFileAndOutput(contactFile);
 
         ArrayList<String> contact = new ArrayList<>();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(new File(String.valueOf(contactFile))));
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-//        contact.add("Naysa, 1234567890");
-//        contact.add("Benny, 2106307421");
-//        contact.add("Austin, 8178465508");
-
         //call the write method to add information to the contacts list
 //        benny.write(contactFile, contact, StandardOpenOption.APPEND);
 
 //        benny.readFileAndOutput(contactFile);
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to your Contacts Menu.");
-        System.out.println("1. View contacts");
-        System.out.println("2. Add a new contact.");
-        System.out.println("3. Search a contact by name.");
-        System.out.println("4. Delete an existing contact.");
-        System.out.println("5. Exit.");
-        System.out.print("Enter a number: ");
-        int userInput = scanner.nextInt();
-        if(userInput == 1) {
-            benny.readFileAndOutput(contactFile);
-        } else if (userInput == 2) {
-            String addContact = scanner.nextLine();
-        } else if (userInput == 3) {
+        int userInput = 0;
+        String addContact = "";
+        while(userInput != 5) {
 
-        } else if (userInput == 4) {
+            System.out.println("Welcome to your Contacts Menu.");
+            System.out.println("1. View contacts");
+            System.out.println("2. Add a new contact.");
+            System.out.println("3. Search a contact by name.");
+            System.out.println("4. Delete an existing contact.");
+            System.out.println("5. Exit.");
+            System.out.print("Enter a number: ");
 
-        } else if (userInput == 5) {
-            System.out.println("Thank you, goodbye!");
+            userInput = scanner.nextInt();
+
+            if (userInput == 1) {
+                benny.readFileAndOutput(contactFile);
+            } else if (userInput == 2) {
+                System.out.println("Enter the contact information:");
+                System.out.println("Name, phoneNumber");
+                System.out.print(":");
+
+                addContact = scanner.nextLine();
+                addContact = scanner.nextLine();
+                contact.add(addContact);
+
+//                benny.write(contactFile, contact, StandardOpenOption.APPEND);
+//                contact.clear();
+
+                benny.addNewContact(contactFile, contact, addContact);
+                contact.clear();
+
+            } else if (userInput == 3) {
+
+            } else if (userInput == 4) {
+
+            } else if (userInput == 5) {
+                System.out.println("Thank you, goodbye!");
+            }
         }
-
-
     }
 }
